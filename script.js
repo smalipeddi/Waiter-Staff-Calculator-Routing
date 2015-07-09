a0b36478bfbc2fe5fdb2c1c32ea6b63ce7a4ebc3
smalipeddi <sunitha_malipeddi@yahoo.com>
2015-07-09 12:29:13 -0400

    Waiter staff calculator using Routing

--- script.js
+++ script.js
@@ -41,97 +41,48 @@ angular.module('MyAPP',['ngRoute'])
 	$scope.name ="sunitha"
 	$scope.md = sharedProperties.mealDetails;
 }).controller('NewMealCtrl',function($scope, sharedProperties){
-	
 	$scope.MealDetails = sharedProperties.mealDetails;
-	console.log($scope.MealDetails);
 	$scope.CutomerCharges = sharedProperties.cutomerCharges;
 	$scope.WaiterEarnings = sharedProperties.waiterEarnings;
-	
-	console.log("I AM HERE ---------------------------sunitha");
-	console.log($scope.CutomerCharges);
-	console.log($scope.WaiterEarnings);
-	
-	/*$scope.WaiterEarnings = sharedProperties.waiterEarnings;
-	*/
-
 	$scope.MealDetails.basePrice = sharedProperties.mealDetails.basePrice;
 	$scope.MealDetails.taxRate = sharedProperties.mealDetails.taxRate;
 	$scope.MealDetails.tipPercent = sharedProperties.mealDetails.tipPercent;
 	$scope.MealDetails.mealCount = sharedProperties.mealDetails.mealCount;
+    var originMealDetailsForm = angular.copy($scope.MealDetails);
+	var origincutomerChargesForm = angular.copy($scope.CutomerCharges);
+	var originWaiterEarningsForm = angular.copy($scope.WaiterEarnings);
 
-
-    /*
-    */
-    $scope.originMealDetailsForm = angular.copy($scope.MealDetails);
-    $scope.origincutomerChargesForm = angular.copy($scope.CutomerCharges);
-    $scope.originWaiterEarningsForm = angular.copy($scope.WaiterEarnings);
-
-    $scope.submit = function() {
-    	if ($scope.myForm.$valid) {
-    		console.log("hello kitty");
-			/*$scope.MealDetails.basePrice = parseInt($scope.MealDetails.basePrice);
-			$scope.MealDetails.taxRate = parseInt($scope.MealDetails.taxRate);
-			$scope.MealDetails.tipPercent = parseInt($scope.MealDetails.tipPercent);
-			*/
-
+	$scope.submit = function() {
+		if ($scope.myForm.$valid) {
 			$scope.CutomerCharges.subTotal = $scope.MealDetails.basePrice + (($scope.MealDetails.taxRate / 100) * $scope.MealDetails.basePrice);
-			console.log("ANUJJJJJJJJJJJJJJJJJJ");
-			console.log($scope.CutomerCharges.subTotal);
-
 			$scope.CutomerCharges.waiterTip = ($scope.CutomerCharges.subTotal * ($scope.MealDetails.tipPercent) / 100);
-			console.log($scope.CutomerCharges.waiterTip);
-			
 			sharedProperties.cutomerCharges.subTotal  = $scope.CutomerCharges.subTotal;
 			sharedProperties.cutomerCharges.waiterTip  = $scope.CutomerCharges.waiterTip;
-			console.log("IMPORTANT");
-			console.log(sharedProperties.cutomerCharges.subTotal);
-			console.log(sharedProperties.cutomerCharges.waiterTip);
-			
-			
-			console.log("waiterTip =" + $scope.CutomerCharges.waiterTip);
 			$scope.CutomerCharges.total = $scope.CutomerCharges.subTotal + $scope.CutomerCharges.waiterTip;
-			console.log("customer total charges" + $scope.CutomerCharges.total);
 			$scope.MealDetails.mealCount = sharedProperties.mealDetails.mealCount + 1 ;
-			console.log("meal count " + $scope.MealDetails.mealCount);
-
-			console.log("Am in new meal");
-			console.log($scope.MealDetails.mealCount);
-
 			sharedProperties.mealDetails.mealCount = $scope.MealDetails.mealCount;
-			console.log(sharedProperties.mealDetails.mealCount);
 			$scope.WaiterEarnings.tipCount = $scope.WaiterEarnings.tipCount + $scope.CutomerCharges.waiterTip;
-			console.log($scope.WaiterEarnings.tipCount);
 			sharedProperties.waiterEarnings.tipCount = $scope.WaiterEarnings.tipCount;
 			$scope.WaiterEarnings.AverageTip = $scope.WaiterEarnings.tipCount / $scope.MealDetails.mealCount;
-		}
+		}        
 
-        /*$scope.reset = function() {
-            $scope.MealDetails = angular.copy($scope.originMealDetailsForm); // Assign clear state to modified form 
-            $scope.CutomerCharges = angular.copy($scope.origincutomerChargesForm);
-            $scope.WaiterEarnings = angular.copy($scope.originWaiterEarningsForm);
-            $scope.myForm.$setPristine();
-        }
+	}
 
-        $scope.reset();*/
+    }).controller('MyEarningsCtrl',function($scope, sharedProperties){
 
-    }
-}).controller('MyEarningsCtrl',function($scope, sharedProperties){
-
-	$scope.WaiterEarnings = sharedProperties.waiterEarnings;
-	
-	//$scope.WaiterEarnings.mealCount = sharedProperties.mealDetails.mealCount;
-   //console.log("Am in earnings");
-   console.log(sharedProperties.waiterEarnings.tipCount);
+   $scope.WaiterEarnings = sharedProperties.waiterEarnings;
    $scope.WaiterEarnings.tipCount = sharedProperties.waiterEarnings.tipCount;
-
-   console.log("meal count " + sharedProperties.mealDetails.mealCount);
-
    $scope.WaiterEarnings.mealCount = sharedProperties.mealDetails.mealCount;
-    /*$scope.WaiterEarnings.tipCount =  sharedProperties.waiterEarnings.tipCount;
-	console.log("initial waiter earnning tip =" + $scope.WaiterEarnings.tipCount);
-	console.log("customer waiter tip = " +  sharedProperties.cutomerCharges.waiterTip);
-	$scope.WaiterEarnings.tipCount = sharedProperties.waiterEarnings.tipCount + sharedProperties.cutomerCharges.waiterTip;
-	console.log($scope.WaiterEarnings.tipCount);*/
-	//$scope.WaiterEarnings.AverageTip = $scope.WaiterEarnings.tipCount / sharedProperties.mealDetails.mealCount;
+    
+    
+    $('#formReset').click(function(){
+    	alert("clicked reset")
+        	$('#myForm')[0].reset();
+            /*$scope.WaiterEarnings = {};
+            $scope.CutomerCharges ={};
+            $scope.MealDetails ={};*/
+
+
+        });
 });
 
